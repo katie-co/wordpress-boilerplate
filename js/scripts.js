5d9f6109b907365f541fb56a115de091b77c4418
Katie Hartrick <6202772+katiehartrick@users.noreply.github.com>
2021-02-22 21:44:41 +0800

    add faAddressBook

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,10 @@
 
 // Import Font Awesome (https://fontawesome.com/how-to-use/on-the-web/advanced/svg-javascript-core)
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
+import { faAddressBook, faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
 
 // We are using a subset of icons
-library.add(faUserAstronaut)
+library.add(faAddressBook, faUserAstronaut)
 
 // Replace any existing <i> tags with <svg> and set up a MutationObserver to
 // continue doing this as the DOM changes.
